1f295073b76669011277788ca31f8a128d314ac3
GhostAutomations <info@ghostautomations.co.uk>
2026-09-19 10:16:42 +0100

    Create form-keys.ts

---
+++ lib/incidents/form-keys.ts
@@ -0,0 +1,3 @@
+export const INCIDENT_REPORT_FORM = "incident_report";
+export const INCIDENT_INVESTIGATION_FORM = "incident_investigation";
+export const INCIDENT_OUTCOME_FORM = "incident_outcome";
